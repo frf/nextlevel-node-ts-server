80cb6f074c26534979d0a8284e0802893ab7d69a
Fabio <fabio@fabiofarias.com.br>
2020-08-17 21:02:22 -0300

    Procfile

---
+++ src/configs/env.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+
+dotenv.config({
+    path: process.env.NODE_ENV === "production" ? ".env.production" : ".env"
+});
+
+export default {
+    DB_CLIENT: process.env.APP_DB_CLIENT || 'mysql',
+    DB_HOST: process.env.APP_DB_HOST || '127.0.0.1',
+    DB_USER: process.env.APP_DB_USER,
+    DB_PASS: process.env.APP_DB_PASS,
+    DB_NAME: process.env.APP_DB_NAME,
+};
